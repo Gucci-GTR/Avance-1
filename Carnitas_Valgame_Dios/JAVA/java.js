dc549a04e39b4b82d7ea656a320a9608d1a33a1d
Gucci-GTR <gustavogalvangtr@gmail.com>
2026-05-30 15:28:15 -0600

    Delete Carnitas_Valgame_Dios directory

--- Carnitas_Valgame_Dios/JAVA/java.js
+++
@@ -1,9 +0,0 @@
-
-let navegacion = document.querySelector('.navegacion')
-
-function scrollfondo(){
-    let scrollTop = document.documentElement.scrollTop;
-    navegacion.style.transform = 'translateY(' * scrollTop * 0.3 + 'px)';
-}
-
-window.addEventListener('scroll',scrollfondo);
